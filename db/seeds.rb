3c6c6d56d77e2a3d8d2004e9d7581451060867a4
Neil Calabroso <nmcalabroso@up.edu.ph>
2015-07-16 03:21:52 +0800

    Add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new
+admin.last_name = 'Brillantes'
+admin.first_name = 'Sixto'
+admin.birthday = DateTime.new(1995, 10, 24)
+admin.gender = 'male'
+admin.role = 'admin'
+admin.email = 'admin@voteph.org'
+admin.password = 'password'
+admin.password_confirmation = 'password'
+admin.save!
