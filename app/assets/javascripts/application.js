621b66bf286b2bb522cce97a424272f5bc40e00e
Mireya Andres <mireyagenandres@gmail.com>
2015-07-18 02:54:24 +0800

    Fix scrolling and isolate fullPage to homepage.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,27 +19,22 @@ $(function(){
 });
 
 $('#return-to-top').click(function() {
-    $('body,html').animate({
-        scrollTop : 0
-    }, 500);
+  $('body,html').animate({
+      scrollTop : 0
+  }, 500);
 });
 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {
-        $('#return-to-top').fadeIn(200);
-    } 
+  if ($(this).scrollTop() >= 50) {
+      $('#return-to-top').fadeIn(200);
+  } 
 
-    else {
-        $('#return-to-top').fadeOut(200);
-    }
+  else {
+      $('#return-to-top').fadeOut(200);
+  }
 });
 
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
-  $('#fullpage').fullpage({
-    scrollBar: true,
-    continuousVertical: false,
-  });
-});
+
